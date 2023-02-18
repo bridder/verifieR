d66c8ec7dd9306826a9c8ea3183c97a39a07350f
bridder <13068790+bridder@users.noreply.github.com>
2023-02-17 22:20:21 -0500

    Cosmetic line break fixes

--- Tests/test_verify_class_and_typeof/test_N_vertype_testedvalue_AT_LEAST_ONE_CLASS_OF_testedvalue_MATCHES_requiredtype.R
+++ Tests/test_verify_class_and_typeof/test_N_vertype_testedvalue_AT_LEAST_ONE_CLASS_OF_testedvalue_MATCHES_requiredtype.R
@@ -3,7 +3,7 @@ test_that(desc = "x_input tibble, but no classes match the three tibble classes.
             {
               require(tidyverse)
               require(magrittr)
-              
+
               MOCK_tibble_length_3_rows_2 <- tibble(x=c(1,2),y=c("omg","bbq"),z=c(99,100))
               error_regex1 <- "^required_classes must all have a match in what is returned by class\\('mt_L3_R2'\\); required_classes: 'integer'; asserted classes: tbl_df, tbl, data\\.frame\\.$"
               error_regex2 <- "^required_classes must all have a match in what is returned by class\\('mt_L3_R2'\\); required_classes: 'ham_sandwich'; asserted classes: tbl_df, tbl, data\\.frame\\.$"
@@ -15,7 +15,7 @@ test_that(desc = "x_input tibble, but no classes match the three tibble classes.
                                                 required_classes = "integer",
                                                 required_typeof = "list"),
                            regexp = error_regex1)
-              
+
               expect_error(object = verify_class_and_typeof(tested_value = MOCK_tibble_length_3_rows_2,
                                                             required_length_min = 3L,
                                                             required_length_max = 3L,
@@ -23,7 +23,7 @@ test_that(desc = "x_input tibble, but no classes match the three tibble classes.
                                                 required_classes = "ham_sandwich",
                                                 required_typeof = "list"),
                            regexp = error_regex2)
-              
+
               expect_error(object = verify_class_and_typeof(tested_value = MOCK_tibble_length_3_rows_2,
                                                             required_length_min = 3L,
                                                             required_length_max = 3L,
