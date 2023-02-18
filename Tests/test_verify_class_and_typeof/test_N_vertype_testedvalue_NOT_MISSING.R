9af22c870189bb00947163b17425cb09931ee11a
bridder <13068790+bridder@users.noreply.github.com>
2023-02-17 23:42:33 -0500

    Refactored and fixed broken tests

--- Tests/test_verify_class_and_typeof/test_N_vertype_testedvalue_NOT_MISSING.R
+++ Tests/test_verify_class_and_typeof/test_N_vertype_testedvalue_NOT_MISSING.R
@@ -5,8 +5,9 @@ test_that(desc = "x_input error if missing.",
               require(magrittr)
 
               expect_error(object = verify_class_and_typeof(#_input = NULL,
-                                                required_length = 9.2,
+                                                required_length_min = 1L,
+                                                required_length_max = 1L,
                                                 tested_value_name =  "fried_eggs",
-                                                required_classes = "integer",
-                                                regexp = "^x_input is missing\\.$"))
+                                                required_classes = "integer"),
+                                                regexp = "^tested_value is missing\\.$")
                            })
--- Tests/test_verify_class_and_typeof/test_N_vertype_testedvalue_NOT_NULL.R
+++ Tests/test_verify_class_and_typeof/test_N_vertype_testedvalue_NOT_NULL.R
@@ -1,12 +1,14 @@
-test_that(desc = "x_input error if NULL.",
+test_that(desc = "x_input error if missing.",
           code =
             {
               require(tidyverse)
               require(magrittr)
 
               expect_error(object = verify_class_and_typeof(tested_value = NULL,
-                                                required_length = 9.2,
-                                                tested_value_name =  "fried_eggs",
-                                                required_classes = "integer",
-                                                regexp = "^x_input cannot be NULL\\.$"))
-                           })
+                                                            required_length_min = 1L,
+                                                            required_length_max = 1L,
+                                                            tested_value_name =  "NULL_tested_value_mock_input",
+                                                            required_classes = "integer",
+                                                            required_typeof = "integer"),
+                           regexp = "^NULL_tested_value_mock_input cannot be NULL\\.$")
+            })
