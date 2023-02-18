c85ec225cf6427825287c1bed0fbf527814d84ea
bridder <13068790+bridder@users.noreply.github.com>
2023-02-17 21:43:01 -0500

    refactored required length arguments

--- Tests/test_verify_class_and_typeof/test_N_vertype_MULTIPLE_requiredclasses_BUT_ASSERTION_IS_MISSING_SOME_BUT_NOT_ALL.R
+++ Tests/test_verify_class_and_typeof/test_N_vertype_MULTIPLE_requiredclasses_BUT_ASSERTION_IS_MISSING_SOME_BUT_NOT_ALL.R
@@ -6,7 +6,8 @@ test_that(desc = "required_type is of length 2L.",
 
               error_regex <- "^required_classes must all have a match in what is returned by class\\('camelCase'\\); required_classes: 'INVALID_data.frame, tbl'; asserted classes: tbl_df, tbl, data\\.frame\\.$"
               expect_error(object = verify_class_and_typeof(tested_value = tibble(x = 1.3,y = 2L,z = "hmm"),
-                                                            required_length = 3L,
+                                                            required_length_min = 3L,
+                                                            required_length_max = 3L,
                                                             tested_value_name =  "camelCase",
                                                             required_classes = c("INVALID_data.frame","tbl"),
                                                             required_typeof = "list"),
