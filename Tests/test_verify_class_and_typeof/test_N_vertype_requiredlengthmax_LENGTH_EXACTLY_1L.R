7cf083136e51132037cce55165e58bc5a635f5ca
bridder <13068790+bridder@users.noreply.github.com>
2023-02-17 21:59:16 -0500

    Added new files first commit of them

---
+++ Tests/test_verify_class_and_typeof/test_N_vertype_requiredlengthmax_LENGTH_EXACTLY_1L.R
@@ -0,0 +1,41 @@
+test_that(desc = "required_length_max is of length 2L; one element is the correct length of 1L.",
+          code =
+            {
+              require(tidyverse)
+              require(magrittr)
+
+              expect_error(object = verify_class_and_typeof(tested_value = TRUE,
+                required_length_min = 1L,
+                required_length_max = c(1L,5L),
+                                                tested_value_name =  "camelCase",
+                                                required_classes = "logical",
+                                                regexp = "^required_length_max must be of length 1L; asserted length = 2L\\.$"))
+            })
+
+test_that(desc = "required_length_max is of length 5L; all elements are the correct length of 1L.",
+          code =
+            {
+              require(tidyverse)
+              require(magrittr)
+
+              expect_error(object = verify_class_and_typeof(tested_value = TRUE,
+                required_length_min = 1L,
+                required_length_max = c(1L,1L,1L,1L,1L),
+                                                tested_value_name =  "camelCase",
+                                                required_classes = "logical",
+                                                regexp = "^required_length_max must be of length 1L; asserted length = 5L\\.$"))
+            })
+
+test_that(desc = "required_length_max is of length 5L; no elements are the correct length of 1L.",
+          code =
+            {
+              require(tidyverse)
+              require(magrittr)
+
+              expect_error(object = verify_class_and_typeof(tested_value = TRUE,
+                required_length_min = 1L,
+                required_length_max = c(12L,44L,0L,-12L,333L),
+                                                tested_value_name =  "camelCase",
+                                                required_classes = "logical",
+                                                regexp = "^required_length_max must be of length 1L; asserted length = 5L\\.$"))
+            })
---
+++ Tests/test_verify_class_and_typeof/test_N_vertype_requiredlengthmin_NOT_MISSING.R
@@ -0,0 +1,13 @@
+test_that(desc = "required_length is not missing.",
+          code =
+            {
+              require(tidyverse)
+              require(magrittr)
+
+              expect_error(object = verify_class_and_typeof(tested_value = TRUE,
+                                                #required_length_min = integer(),
+                                                required_length_max = 1L,
+                                                tested_value_name =  "camelCase",
+                                                required_classes = "logical",
+                                                regexp = "^required_length_min is missing\\.$"))
+            })
